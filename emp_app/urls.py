af359b3a5fe9c8997c977c0bd43ce91094c23f04
abhinav0000 <abhisavinav123@gmail.com>
2022-07-26 22:45:50 +0530

    errors removed

--- emp_app/urls.py
+++ emp_app/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
     path('add_emp', views.add_emp, name='add_emp'),
     path('all_emp', views.all_emp, name='all_emp'),
     path('remove_emp', views.remove_emp, name='remove_emp'),
